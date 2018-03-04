318c1880a5e9f1a09642a386509c0b604936646e
Eduardo Elias <camponez@gmail.com>
2018-03-04 17:58:00 +0000

    Version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# eostool
-EOS scripting tools
+## Setup
+
+This script assume you are not running `eosiod` inside the [eos build directory](https://github.com/EOSIO/eos/tree/master#build).
+
+### Suggestion
+
+Run `eosid` in a separate directory like `$HOME/eos-programs`
+
+```bash
+mkdir $HOME/eos-programs/
+cd $HOME/eos-programs/
+git clone  https://github.com/camponez/eostool.git tools
+cd $HOME/eos-programs
+# pass the FULL PATH where EOS was built
+./tools/eosio-tools.sh setup $HOME/eos/build
+```
+
+## Usage
+* `./tools/eosio-tools.sh start` - start eosiod and redirect the log to `eosiod.log`
+* `./toos/eosio-tools.sh stop` - stop eosiod
+* `./toos/eosio-tools.sh restart` - restart eosid. **NOTE**: This will overwrite the `eosid.log.old` file. Save it before using this option
+
+## Seeing the logs
+
+`eosiod` will be running in the background. In order to see the log, just run:
+```bash
+tail -f eosiod.log
+```
+
+`CTRL+c` will stop tail but not `eosiod`.
---
+++ eosio-tools.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+LOG_FILE='eosiod.log'
+PID_FILE='eosiod.pid'
+VERSION='0.1'
+
+function start_eosio {
+
+    if [ -f "${PID_FILE}" ]; then
+        echo 'Looks like eosiod is running. Use stop command first.'
+        echo "If you are sure eosiod is not running delete ${PID_FILE}"
+        exit 0
+    fi
+
+    if [ -d './data-dir' ]; then
+        if [ -f "${LOG_FILE}" ]; then
+            echo 'Backing up the log file...'
+            mv "${LOG_FILE}" "${LOG_FILE}.old"
+        fi
+
+        echo 'Starting eosiod...';
+        ./eosiod > ${LOG_FILE} 2>&1 &
+        echo $! >${PID_FILE};
+    else
+        echo 'data-dir/ has to be present in the same directory as eosiod'
+    fi
+}
+
+function stop_eosio {
+    if [ -f ${PID_FILE} ]; then
+        echo 'Stopping...'
+        kill -15 `cat eosid.pid`
+        rm -v ${PID_FILE}
+    else
+        echo "No ${PID_FILE} found"
+    fi
+}
+
+function setup {
+    EOS="$1"
+    cp -v "$EOS/programs/eosiod/eosiod" .
+    echo 'Now run `./tools/start.sh start` to start your eosiod'
+}
+
+case $1 in
+    "start")
+        start_eosio
+        ;;
+    "stop")
+        stop_eosio
+        ;;
+
+    "restart")
+        stop_eosio
+        sleep 5
+        start_eosio
+        ;;
+
+    "setup")
+        if [ -d "$2" ]; then
+            setup "$2"
+        else
+            echo "$2 does not exist. Please pass the FULL PATH"
+            echo "usage: ./tools/start.sh setup $HOME/eos/build"
+        fi
+        ;;
+    "version")
+        echo "Version $VERSION"
+        ;;
+    *)
+        echo 'Options are "start" and "stop"'
+
+    esac
